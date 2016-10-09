89fee8eb3cf3ad70a3d506b2f10da4456a6e10c2
root <root@redmine-test.multiprojet>
2016-10-09 08:27:18 +0000

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :iconist do
+  name 'Iconist plugin'
+  author 'Jerome Rivron'
+  description 'Add Icon for issue'
+  version '0.0.1'
+  url 'https://github.com/jrivron/redmine_iconsit'
+  author_url 'https://github.com/jrivron'
+end
